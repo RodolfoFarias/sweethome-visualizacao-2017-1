a8691b85f1db67e28b5e23a407e336323d5d47d3
RodolfoFarias <jrlf9@hotmail.com>
2017-05-08 23:11:05 -0300

    heatmap

--- D3/main.js
+++ D3/main.js
@@ -5,7 +5,7 @@ window.onload = function(){
 
 
 var init =  function(){
-	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	var mymap = L.map('mapid').setView([-8.0620287, -34.8987418], 13);
 
 	mapLink = '<a href="https://carto.com/attribution">Carto</a>';
     L.tileLayer(
@@ -13,4 +13,17 @@ var init =  function(){
         attribution: '&copy; ' + mapLink + ' Contributors',
         maxZoom: 18,
     }).addTo(mymap);
+
+    
+    d3.json("RecifeTransportation.geojson", function(data){
+    	coordinates = [];
+    	for (i = 0; i < data.features.length ; i++) {
+    		tempCoor = [data.features[i].geometry.coordinates[1],data.features[i].geometry.coordinates[0]]
+    		coordinates.push(tempCoor)
+    	}
+
+    	var heat = L.heatLayer(coordinates, {radius: 25}).addTo(mymap);  // lat, lng, intensity
+	
+    })
+    
 }
