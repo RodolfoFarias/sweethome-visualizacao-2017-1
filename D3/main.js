b711e5043adb666574d9905a4c89633c4f06c3a4
Nivan Ferreira <nivan.ferreira@gmail.com>
2017-05-16 15:09:34 -0300

    conversa sobre criar heatmap

--- D3/main.js
+++ D3/main.js
@@ -2,7 +2,17 @@ window.onload = function(){
 	init();
 }
 
-
+// function measure(lat1, lon1, lat2, lon2){  // generally used geo measurement function
+//     var R = 6378.137; // Radius of earth in KM
+//     var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+//     var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+//     var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+//     Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+//     Math.sin(dLon/2) * Math.sin(dLon/2);
+//     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+//     var d = R * c;
+//     return d * 1000; // meters
+// }
 
 var init =  function(){
 	var mymap = L.map('mapid').setView([-8.0620287, -34.8987418], 13);
@@ -16,7 +26,7 @@ var init =  function(){
     
 
 
-    var grid = drawGrid(-8.0620287, -34.8987418, 53.902257, 27.561640, 100, mymap);
+    var grid = drawGrid(-8.115846, -34.998665, -7.951308, -34.774132, 100, mymap);
 
     debugger;
 }
---
+++ Python/createHeatmaps.py
@@ -0,0 +1,11 @@
+Recife ->         Heatmap Heathcare
+tamanhoGrid       Heatmap Financial
+raio              Heatmap Transportation
+                  ...
+
+para cada celula (cx,cy), pegue os pontos contidos num raio (de tamanho raio) do centro.
+Decaimento linear de 1 para 0. funcao de decaimento do centro ate o raio.
+
+Salve os counts para cada celula.
+
+Ligue isso com a função de grid com uma escala de cores do colorbrewer.
