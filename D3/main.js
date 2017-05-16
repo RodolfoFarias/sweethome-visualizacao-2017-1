473435073ea869592b9934cfec650d463ad437c9
RodolfoFarias <jrlf9@hotmail.com>
2017-05-15 22:45:23 -0300

    grid function

--- D3/main.js
+++ D3/main.js
@@ -13,17 +13,39 @@ var init =  function(){
         attribution: '&copy; ' + mapLink + ' Contributors',
         maxZoom: 18,
     }).addTo(mymap);
-
     
-    d3.json("RecifeTransportation.geojson", function(data){
-    	coordinates = [];
-    	for (i = 0; i < data.features.length ; i++) {
-    		tempCoor = [data.features[i].geometry.coordinates[1],data.features[i].geometry.coordinates[0]]
-    		coordinates.push(tempCoor)
-    	}
 
-    	var heat = L.heatLayer(coordinates, {radius: 25}).addTo(mymap);  // lat, lng, intensity
-	
-    })
-    
+
+    var grid = drawGrid(-8.0620287, -34.8987418, 53.902257, 27.561640, 2, mymap);
+
+    debugger;
 }
+
+var drawGrid = function(lat_min, long_min, lat_max, long_max, cells, mymap){
+
+	var widthTotal =  Math.abs(lat_min - lat_max)
+	var heightTotal = Math.abs(long_min - long_max)
+	var eachWidth = widthTotal/cells
+	var eachHeight = heightTotal/cells
+	var grid = new Array();
+
+		for(var row = 0; row < cells; row++){
+			grid.push( new Array())
+
+			for(var column = 0; column < cells; column++){
+
+				var bounds = [
+							 [lat_min + column * eachWidth, long_min + row * eachHeight], 
+							 [lat_min + (column + 1) * eachWidth, long_min + (row + 1) * eachHeight]
+							 										];
+				var rect = L.rectangle(bounds, 
+					{color: d3.rgb(d3.randomUniform(255)(),d3.randomUniform(255)(),d3.randomUniform(255)()), weight: 0}).
+					on('click', function (e) {
+
+   					console.info(e);
+				}).addTo(mymap);
+			}
+				grid[row].push(rect);
+		}	
+	return grid	
+}	
