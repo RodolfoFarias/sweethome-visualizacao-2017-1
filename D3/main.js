f2e9b4d1f3a439b539662cca9b6ca0b902ca5f1a
RodolfoFarias <jrlf9@hotmail.com>
2017-05-21 01:07:24 -0300

    Heatmap para education em recife

--- D3/main.js
+++ D3/main.js
@@ -28,7 +28,17 @@ var init =  function(){
 
     var grid = drawGrid(-8.115846, -34.998665, -7.951308, -34.774132, 100, mymap);
 
+
+    /*
+    for (var i = 0; i < grid.length; i++) {
+    	for (var j = 0; j < grid[i].length; j++) {
+    		//id, lat, long
+    		console.log(grid[i][j]._leaflet_id + ": [" + grid[i][j].getCenter().lat + ", " + grid[i][j].getCenter().lng + "],")
+    	}
+    }
+
     debugger;
+    */
 }
 
 var drawGrid = function(lat_min, long_min, lat_max, long_max, cells, mymap){
@@ -38,24 +48,33 @@ var drawGrid = function(lat_min, long_min, lat_max, long_max, cells, mymap){
 	var eachWidth = widthTotal/cells
 	var eachHeight = heightTotal/cells
 	var grid = new Array();
+	var cor = d3.scaleLinear()
+		.range([0,1])
+		.domain([d3.min(pythonOut, function(d){return d.hits}), d3.max(pythonOut, function(d){return d.hits})]);
 
 		for(var row = 0; row < cells; row++){
 			grid.push( new Array())
 
 			for(var column = 0; column < cells; column++){
 
+
 				var bounds = [
 							 [lat_min + column * eachWidth, long_min + row * eachHeight], 
 							 [lat_min + (column + 1) * eachWidth, long_min + (row + 1) * eachHeight]
+
+
 							 										];
 				var rect = L.rectangle(bounds, 
-					{color: d3.rgb(d3.randomUniform(255)(),d3.randomUniform(255)(),d3.randomUniform(255)()), weight: 0}).
+					{weight: 0}).
 					on('click', function (e) {
 
    					console.info(e);
-				}).addTo(mymap);
-			}
+				});
+				rect.options.color = d3.interpolateBlues(cor(pythonOut[(row * 100) + column].hits))
+				rect.addTo(mymap)	
 				grid[row].push(rect);
+			}
+				
 		}	
 	return grid	
 }	
---
+++ Python/heatmap.py
@@ -0,0 +1,43 @@
+#C:/Users/Rodolfo/Documents/GitHub/sweethome-visualizacao-2017-1/Python/RecifeTransportation.geojson
+import geojson
+from geopy.distance import vincenty
+
+def calcDist(lat1, long1, lat2, long2):
+    #vincenty (lat,long) , (lat,long)
+    
+    return vincenty((lat1, long1), (lat2, long2)).meters
+
+#Sections:
+#   Education
+#   Entertainment
+#   Financial
+#   Healthcare
+#   Others
+#   Sustenance
+#   Transportation
+
+def hits(lat, long, radius, section):
+    path = "C:/Users/Rodolfo/Documents/GitHub/sweethome-visualizacao-2017-1/Python/data/recife/Recife" + section + ".geojson"
+    json_data=open(path).read()
+    data = geojson.loads(json_data)
+    counts = 0;
+
+    for x in data.features:
+    
+        dist = calcDist(lat, long, x.geometry.coordinates[0], x.geometry.coordinates[1])
+        if dist < radius:
+            counts = counts + (radius - dist)/radius
+    	
+    return counts
+
+dict = eval(open('C:/Users/Rodolfo/Documents/GitHub/sweethome-visualizacao-2017-1/Python/data/recife/gridOutput.log', 'r').read())
+
+
+f = open('pythonOutPut', 'w')
+
+for keys in dict:
+    lat = dict.get(keys)[0]
+    long = dict.get(keys)[1]
+    f.write("{id: " + str(keys) + ", hits :" + str(hits(long, lat, 1000, "Education")) + "},\n")
+
+f.close();
